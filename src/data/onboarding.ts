cfc644287fb2bab02a250cd36481bea71f187d0f
GABLRM <g.laroumanie@gmail.com>
2024-04-09 11:40:37 +0200

    ::sparkles: feat: add data for onboarding + colors util

---
+++ src/data/onboarding.ts
@@ -0,0 +1,36 @@
+import { AnimationObject } from "lottie-react-native";
+import { Colors } from "../utils/colors";
+
+export interface onBoardingInterface {
+    id: number;
+    animation: AnimationObject;
+    text: string;
+    textColor: string;
+    backgroundColor: string;
+}
+
+const onBoardingData: onBoardingInterface[] = [
+    {
+        id: 1,
+        animation: require('../assets/animations/animation1.json'),
+        text: 'Welcome to the App',
+        textColor: Colors.ORANGE,
+        backgroundColor: Colors.PINK,
+    },
+    {
+        id: 2,
+        animation: require('../assets/animations/animation2.json'),
+        text: 'This is the second screen',
+        textColor: Colors.PINK,
+        backgroundColor: Colors.GREEN,
+    },
+    {
+        id: 3,
+        animation: require('../assets/animations/animation3.json'),
+        text: 'This is the third screen',
+        textColor: Colors.GREEN,
+        backgroundColor: Colors.WHITE,
+    },
+];
+
+export default onBoardingData;
---
+++ src/utils/colors.ts
@@ -0,0 +1,7 @@
+export const Colors = {
+    BLUE: '#5bc4f9',
+    GREEN: '#9cfa19',
+    PINK: '#ff0066',
+    ORANGE: '#ffde00',
+    WHITE: '#E7E7E7'
+}
