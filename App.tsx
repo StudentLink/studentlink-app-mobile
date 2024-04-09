988d62565b205587051bd31e1ccf6dd78551e646
GABLRM <g.laroumanie@gmail.com>
2024-04-09 16:50:51 +0200

    :sparkles: feat: onboarding with animations

--- App.tsx
+++ App.tsx
@@ -1,15 +1,28 @@
 import {StyleSheet, View } from 'react-native';
-import onBoardingData from './src/data/onboarding';
+import onBoardingData, { onBoardingInterface } from './src/data/onboarding';
 import RenderItem from './src/components/RenderItem';
-import Animated from 'react-native-reanimated';
+import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import { FlatList } from 'react-native-reanimated/lib/typescript/Animated';
 
 const App = () => {
+  const flatListRef = useAnimatedRef<FlatList<onBoardingInterface>>();
+  const x = useSharedValue(0);
+
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll:event => {
+      x.value = event.contentOffset.x;
+    }
+  })
+
   return (
     <View style={styles.container}>
       <Animated.FlatList 
+        ref={flatListRef}
+        onScroll={onScroll}
         data={onBoardingData} 
         renderItem={({ item, index }) => {
-          return <RenderItem item={item} index={index}/>;
+          return <RenderItem item={item} index={index} x={x}/>;
       }}
       keyExtractor={item => item.id}
       scrollEventThrottle={16}
