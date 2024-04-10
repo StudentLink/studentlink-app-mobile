ca714b3b71714eb41760b54ad38de41f5e907805
GABLRM <g.laroumanie@gmail.com>
2024-04-10 22:46:04 +0200

    :sparkles: feat: pagination

--- App.tsx
+++ App.tsx
@@ -1,36 +1,56 @@
-import {StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ViewToken } from 'react-native';
 import onBoardingData, { onBoardingInterface } from './src/data/onboarding';
 import RenderItem from './src/components/RenderItem';
 import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
 import { FlatList } from 'react-native-reanimated/lib/typescript/Animated';
+import Pagination from './src/components/pagination/Pagination';
 
 const App = () => {
   const flatListRef = useAnimatedRef<FlatList<onBoardingInterface>>();
   const x = useSharedValue(0);
+  const flatListIndex = useSharedValue(0);
+
+  const onViewableItemsChanged = ({
+    viewableItems
+  }: {
+    viewableItems: ViewToken[];
+  }) => {
+    if (viewableItems[0].index !== null) {
+      flatListIndex.value = viewableItems[0].index;
+    }
+  };
 
 
   const onScroll = useAnimatedScrollHandler({
-    onScroll:event => {
+    onScroll: event => {
       x.value = event.contentOffset.x;
     }
   })
 
   return (
     <View style={styles.container}>
-      <Animated.FlatList 
+      <Animated.FlatList
         ref={flatListRef}
         onScroll={onScroll}
-        data={onBoardingData} 
+        data={onBoardingData}
         renderItem={({ item, index }) => {
-          return <RenderItem item={item} index={index} x={x}/>;
-      }}
-      keyExtractor={item => item.id}
-      scrollEventThrottle={16}
-      horizontal={true}
-      bounces={false}
-      pagingEnabled={true}
-      showsHorizontalScrollIndicator={false}
+          return <RenderItem item={item} index={index} x={x} />;
+        }}
+        keyExtractor={item => item.id}
+        scrollEventThrottle={16}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{
+          minimumViewTime: 300,
+          viewAreaCoveragePercentThreshold: 10,
+        }}
       />
+      <View style={styles.bottomContainer}>
+        <Pagination data={onBoardingData} x={x} />
+      </View>
     </View>
   );
 }
@@ -42,6 +62,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    marginHorizontal: 30,
+    paddingVertical: 30,
+  }
 });
 
 export default App;
