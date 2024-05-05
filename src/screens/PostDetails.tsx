deda02127501fb1678e70ccf2f0c2fad86899e41
GABLRM <g.laroumanie@gmail.com>
2024-05-05 02:19:18 +0200

    :sparkles: feat: input comment and change things

--- src/screens/PostDetails.tsx
+++ src/screens/PostDetails.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, KeyboardAvoidingView, Pressable, TouchableHighlight, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Colors } from '../utils/colors'
 import { useNavigation, useRoute } from '@react-navigation/native'
@@ -8,8 +8,13 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import BackButton from '../components/Button/BackButton'
 import * as SecureStore from 'expo-secure-store'
 import Comment from '../components/Comment'
-import { ScrollView } from 'react-native-gesture-handler'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
 import CommentType from '../data/customTypes/Comment'
+import { Ionicons } from '@expo/vector-icons/'
+import { CapitalizeData } from '../utils/verification'
+import City from '../data/customTypes/City'
+import CityJson from '../data/cities.json';
+
 
 const PostDetails = () => {
 
@@ -17,6 +22,7 @@ const PostDetails = () => {
   const { post } = route.params as { post: PostType };
 
   const [comments, setComments] = useState<CommentType[]>([]);
+  const [userComment, setUserComment] = useState('');
 
   const navigation = useNavigation();
 
@@ -36,6 +42,35 @@ const PostDetails = () => {
     }
   }
 
+  const addComment = async () => {
+    try {
+      const response = await fetch(`https://studentlink.etudiants.ynov-bordeaux.com/api/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + (await SecureStore.getItemAsync('token'))
+        },
+        body: JSON.stringify({
+          content: userComment,
+          post: post.id,
+        })
+      });
+      const json = await response.json();
+      console.log(json);
+      setUserComment('');
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getLocalisationName = (inseeCode: number) => {
+    const result = (CityJson as City[]).find((city) => (parseInt(city.insee_code) == inseeCode));
+    if (result) {
+      return CapitalizeData(result.label);
+    }
+    return 'N/A';
+  }
+
   useEffect(() => {
     getComments();
   }, []);
@@ -43,30 +78,30 @@ const PostDetails = () => {
   return (
     <SafeAreaView style={styles.container}>
       <BackButton onclick={() => navigation.goBack()} />
-      <View style={styles.post}>
-        <View style={styles.userInfo}>
-          <Image src={`https://ui-avatars.com/api/?format=png&size=512&rounded=true&name=${post.user.name.replaceAll(/[ -'_]+/g, '+')}`} style={styles.picture} />
-          <View style={styles.infoData}>
-            <View style={styles.userPost}>
-              <Text style={styles.name}>{post.user.name}</Text>
-              <Text style={styles.username}>@{post.user.username}</Text>
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1, width: "100%" }}>
+        <View style={styles.post}>
+          <View style={styles.userInfo}>
+            <Image src={`https://ui-avatars.com/api/?format=png&size=512&rounded=true&name=${post.user.name.replaceAll(/[ -'_]+/g, '+')}`} style={styles.picture} />
+            <View style={styles.infoData}>
+              <View style={styles.userPost}>
+                <Text style={styles.name}>{post.user.name}</Text>
+                <Text style={styles.username}>@{post.user.username}</Text>
+              </View>
+              <Text style={styles.school}>{post.school ? post.school.name : getLocalisationName(post.location)}</Text>
             </View>
-            <Text style={styles.school}>{post.location}</Text>
+          </View>
+          <View style={{ flex: 1, height: 1, backgroundColor: Colors.WHITE, marginVertical: 15, opacity: 0.1 }} />
+          <View>
+            <Text style={styles.content}>{post.content}</Text>
+            <View style={styles.footer}>
+              <Text style={styles.footerData}>{(post.comments.length == 0) ? "Ajouter un commentaire" : `${post.comments.length} commentaire${post.comments.length > 1 ? 's' : ''}`}</Text>
+              <Text style={styles.footerData}>{formatDate(post.createdAt)}</Text>
+            </View>
           </View>
         </View>
-        <View style={{ flex: 1, height: 1, backgroundColor: Colors.WHITE, marginVertical: 15, opacity: 0.1 }} />
-        <View>
-          <Text style={styles.content}>{post.content}</Text>
-          <View style={styles.footer}>
-            <Text style={styles.footerData}>{(post.comments.length == 0) ? "Ajouter un commentaire" : `${post.comments.length} commentaire${post.comments.length > 1 ? 's' : ''}`}</Text>
-            <Text style={styles.footerData}>{formatDate(post.createdAt)}</Text>
-          </View>
-        </View>
-      </View>
-      {comments.length > 0 ? (
-        <ScrollView style={styles.scroll}>
-          {
-            comments?.map((comment, index) => (
+        {comments.length > 0 ? (
+          <><ScrollView style={styles.scroll}>
+            {comments?.map((comment, index) => (
               <Comment
                 key={index}
                 content={comment.content}
@@ -74,10 +109,24 @@ const PostDetails = () => {
                 name={comment.user.name}
                 username={comment.user.username}
                 profilePicture={`https://ui-avatars.com/api/?format=png&size=512&rounded=true&name=${comment.user.name.replaceAll(/[ -'_]+/g, '+')}`} />
-            ))
-          }
-        </ScrollView>
-      ) : <Text style={styles.noComment}>Aucun commentaire</Text>}
+            ))}
+          </ScrollView>
+            <View style={styles.input}>
+              <TextInput style={styles.addComment} placeholder="Ajouter un commentaire" placeholderTextColor={Colors.WHITE} onChangeText={setUserComment} multiline={true} value={userComment} />
+              <TouchableOpacity style={styles.button} onPress={() => addComment()}>
+                <Ionicons name="send" size={15} color={Colors.BLUE} />
+              </TouchableOpacity>
+            </View>
+          </>
+        ) : <><Text style={styles.noComment}>Aucun commentaire</Text><View style={styles.input}>
+          <TextInput style={styles.addComment} placeholder="Ajouter un commentaire" placeholderTextColor={Colors.WHITE} onChangeText={setUserComment} multiline={true} value={userComment} />
+          <TouchableOpacity style={styles.button} onPress={() => addComment()}>
+            <Ionicons name="send" size={15} color={Colors.BLUE} />
+          </TouchableOpacity>
+        </View></>
+        }
+
+      </KeyboardAvoidingView>
     </SafeAreaView>
   )
 }
@@ -156,9 +205,36 @@ const styles = StyleSheet.create({
   },
   noComment: {
     flex: 1,
+    textAlign: 'center',
     color: Colors.WHITE,
     marginTop: 20,
     fontSize: 20,
     opacity: 0.5,
-  }
+  },
+  addComment: {
+    backgroundColor: Colors.DARKEN_BLUE_LIGHTER,
+    color: Colors.WHITE,
+    width: '80%',
+    marginRight: 20,
+    borderColor: Colors.BLUE,
+    borderWidth: 1,
+    fontSize: 20,
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 20,
+  },
+  input: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  button: {
+    backgroundColor: Colors.DARKEN_BLUE_LIGHTER,
+    borderRadius: 100,
+    width: 40,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 })
