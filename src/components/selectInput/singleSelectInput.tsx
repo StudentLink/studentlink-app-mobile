05452abd3aee2138e018e643ce2f9df099238ef1
GABLRM <g.laroumanie@gmail.com>
2024-05-07 00:25:14 +0200

    :bug: fix: selectsingleinput

--- src/components/selectInput/singleSelectInput.tsx
+++ src/components/selectInput/singleSelectInput.tsx
@@ -24,7 +24,7 @@ const SingleSelectInput = ({ data, onChange, icon }: Props) => {
                         name="checkmark"
                         size={25}
                     />
-                )};
+                )}
             </View>
         );
     };
